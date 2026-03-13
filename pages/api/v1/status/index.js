ff524d96bbc461b0a190f57489eedadd75858962
Gustavo 442 <demourasilvagustavo@gmail.com>
2026-03-13 15:04:34 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  try {
+    const updatedAt = new Date().toISOString();
+
+    const version = await database.query("SHOW server_version;");
+    const maxConnections = await database.query("SHOW max_connections;");
+
+    const databaseName = process.env.POSTGRES_DB;
+    const openedConnections = await database.query({
+      text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    });
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
+        version: version.rows[0].server_version,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    response.status(500).json({
+      message: "Internal error!",
+    });
+  }
 }
 
-export default status = status;
+export default status;
